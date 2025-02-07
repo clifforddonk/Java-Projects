81478d4ca00aae832f42cebed04fd5ae7da2baf5
clifforddonk <clifforddonk@gmail.com>
2025-02-07 11:16:03 +0000

    first commit

---
+++ src/com/clifford/Clifford.java
@@ -0,0 +1,15 @@
+package com.clifford;
+
+public class Clifford {
+    public static void main(String[] args) {
+        System.out.println("Heyyyyy");
+        sayHi();
+    }
+
+    public static void sayHi() {
+        System.out.println("Hi from Clifford");
+
+
+    }
+
+}
