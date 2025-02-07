4aab75cf37d621ce33c80e3e06f135535f0c5550
clifforddonk <clifforddonk@gmail.com>
2025-02-07 11:32:24 +0000

    first commit

--- src/com/clifford/Clifford.java
+++ src/com/clifford/Clifford.java
@@ -4,6 +4,7 @@ public class Clifford {
     public static void main(String[] args) {
         System.out.println("Heyyyyy");
         sayHi();
+        Main.Hey(12);
     }
 
     public static void sayHi() {
--- src/com/clifford/Main.java
+++ src/com/clifford/Main.java
@@ -3,6 +3,10 @@ package com.clifford;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world of developers");
+        Clifford.sayHi();
     }
 
+    public static void Hey(int age) {
+        System.out.println("I am "+ age +" years old");
+    }
 }
