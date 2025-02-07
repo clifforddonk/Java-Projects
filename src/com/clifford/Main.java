b0b14c31a9010e00d0757be8370fbfc7d8445005
clifforddonk <clifforddonk@gmail.com>
2025-02-07 09:11:30 +0000

    first commit

--- src/com/clifford/Main.java
+++ src/com/clifford/Main.java
@@ -2,7 +2,7 @@ package com.clifford;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello people");
+        System.out.println("Hello world of Java developers");
     }
 
 }
